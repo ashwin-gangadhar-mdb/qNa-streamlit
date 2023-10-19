9dbd3c963dc62a26abe12ba6ce36c6193a9349d4
Venkatesh Shanbhag <91714892+theshanbhag@users.noreply.github.com>
2023-10-19 19:15:33 +0530

    initial commit

--- run.py
+++ run.py
@@ -3,12 +3,12 @@ from streamlit_extras.add_vertical_space import add_vertical_space
 
 from PyPDF2 import PdfReader
 
-from langchain.llms import LlamaCpp,OpenAI
+from langchain.llms import LlamaCpp, OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import MongoDBAtlasVectorSearch
-from langchain.text_splitter import RecursiveCharacterTextSplitter,CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
 from langchain.chains import RetrievalQAWithSourcesChain, RetrievalQA, ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from InstructorEmbedding import INSTRUCTOR
@@ -19,23 +19,25 @@ from tqdm import tqdm
 
 from pymongo import MongoClient
 
-
 import os
 
-os.environ["MONGO_CONNECTION_STR"] = "mongodb+srv://ashwin:pswd123ash@retail-demo.2wqno.mongodb.net/?retryWrites=true&w=majority"
+os.environ[
+    "MONGO_CONNECTION_STR"] = "mongodb+srv://ashwin:pswd123ash@retail-demo.2wqno.mongodb.net/?retryWrites=true&w=majority"
 os.environ["SENTENCE_TRANSFORMERS_HOME"] = "tmp/st/"
 
 client = MongoClient(os.getenv("MONGO_CONNECTION_STR"))
 db = client["sample"]
 
-one_way_hash = lambda x:hashlib.md5(x.encode("utf-8")).hexdigest()
+one_way_hash = lambda x: hashlib.md5(x.encode("utf-8")).hexdigest()
+
 
 def check_doc_in_mdb(md5):
-    if len(list(db["chatverify"].find({"md5":md5})))>0:
+    if len(list(db["chatverify"].find({"md5": md5}))) > 0:
         return True
     else:
-        db["chatverify"].insert_one({"md5":md5})
+        db["chatverify"].insert_one({"md5": md5})
         return False
+
 
 def get_pdf_data(pdf_docs):
     for pdf in pdf_docs:
@@ -59,6 +61,7 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
+
 def get_emb_trnsformers():
     emb_model = "sentence-transformers/all-MiniLM-L6-v2"
     embeddings_w = HuggingFaceEmbeddings(
@@ -69,13 +72,15 @@ def get_emb_trnsformers():
 
 
 def get_embeddings_transformer():
-    return HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base", model_kwargs={"device":"cpu"})
+    return HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base", model_kwargs={"device": "cpu"})
 
 
 def get_vector_store():
     col = db["chatapp"]
-    vs = MongoDBAtlasVectorSearch(collection=col,embedding=get_embeddings_transformer(),index_name="default",embedding_key="vec", text_key="line")
+    vs = MongoDBAtlasVectorSearch(collection=col, embedding=get_embeddings_transformer(), index_name="default",
+                                  embedding_key="vec", text_key="line")
     return vs
+
 
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI()
@@ -88,6 +93,7 @@ def get_conversation_chain(vectorstore):
         memory=memory
     )
     return conversation_chain
+
 
 def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
@@ -108,7 +114,7 @@ def get_llm_chain():
     #     model_path="models/ggml-model-q4_0.bin", callback_manager=callback_manager, verbose=True,
     #     n_ctx=1024
     # )
-    llm = OpenAI(model_name= "gpt-3.5-turbo-16k",temperature=0.2)
+    llm = OpenAI(model_name="gpt-3.5-turbo-16k", temperature=0.2)
     chain = RetrievalQA.from_chain_type(llm=llm, retriever=get_vector_store().as_retriever())
     return chain
 
@@ -116,8 +122,10 @@ def get_llm_chain():
 def main():
     st.set_page_config(page_title="Chat with multiple PDFs",
                        page_icon=":books:")
-    
-    st.markdown("""<img src="https://webimages.mongodb.com/_com_assets/cms/kuyjf3vea2hg34taa-horizontal_default_slate_blue.svg?auto=format%252Ccompress" class=" css-1lo3ubz" alt="MongoDB logo" style="height:300px;width:500px;align:center"> """, unsafe_allow_html=True)
+
+    st.markdown(
+        """<img src="https://webimages.mongodb.com/_com_assets/cms/kuyjf3vea2hg34taa-horizontal_default_slate_blue.svg?auto=format%252Ccompress" class=" css-1lo3ubz" alt="MongoDB logo" style="height:300px;width:500px;align:center"> """,
+        unsafe_allow_html=True)
     st.title("""Your Q&A Assistant for any source powered by Atlas Vector Search """)
 
     if "conversation" not in st.session_state:
@@ -138,7 +146,7 @@ def main():
         - [Streamlit](https://streamlit.io/)
         - [LangChain](https://python.langchain.com/)
         - [MongoDB Vector Search](https://www.mongodb.com/products/platform/atlas-vector-search)
-    
+
         ''')
         add_vertical_space(3)
         st.subheader("Your documents")
@@ -148,13 +156,13 @@ def main():
             with st.spinner("Processing"):
                 # get pdf text
                 raw_text = get_pdf_data(pdf_docs)
-                
+
                 if raw_text:
                     # get the text chunks
                     text_chunks = get_text_chunks(raw_text)
 
                     for i in tqdm(range(len(text_chunks), 100)):
-                        batch_chunks = text_chunks[i:i+99]
+                        batch_chunks = text_chunks[i:i + 99]
                         # create vector store
                         get_vector_store().add_texts(text_chunks)
 
@@ -163,5 +171,6 @@ def main():
         add_vertical_space(5)
         st.write('Made with ❤️ by [Ashwin Gangadhar](linkedin.com/in/ashwin-gangadhar-00b17046)')
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
