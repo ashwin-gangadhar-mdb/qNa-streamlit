6391aa0ccaed418ad92dff2187260557ba54c654
Ashwin Gangadhar <ashwin.gangadhar@mongodb.com>
2023-10-21 00:15:26 +0530

    updated code parallely to run

--- run.py
+++ run.py
@@ -3,174 +3,189 @@ from streamlit_extras.add_vertical_space import add_vertical_space
 
 from PyPDF2 import PdfReader
 
-from langchain.llms import LlamaCpp, OpenAI
-from langchain.chat_models import ChatOpenAI
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.llms import LlamaCpp, VertexAI
+# Todo change to Vertex AI
+from langchain.chat_models import ChatVertexAI,ChatOpenAI
 from langchain.vectorstores import MongoDBAtlasVectorSearch
-from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
-from langchain.chains import RetrievalQAWithSourcesChain, RetrievalQA, ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
+from langchain.text_splitter import  CharacterTextSplitter
+from langchain.chains import  ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory
+from langchain.embeddings import VertexAIEmbeddings
 from InstructorEmbedding import INSTRUCTOR
 from langchain.embeddings import HuggingFaceInstructEmbeddings, HuggingFaceEmbeddings
-from functools import lru_cache
+from langchain.prompts import PromptTemplate
+
 import hashlib
 from tqdm import tqdm
+from functools import lru_cache
 
 from pymongo import MongoClient
+import certifi
 
 import os
+from dotenv import load_dotenv
 
-os.environ[
-    "MONGO_CONNECTION_STR"] = "mongodb+srv://ashwin:pswd123ash@retail-demo.2wqno.mongodb.net/?retryWrites=true&w=majority"
+load_dotenv()
+
 os.environ["SENTENCE_TRANSFORMERS_HOME"] = "tmp/st/"
 
-client = MongoClient(os.getenv("MONGO_CONNECTION_STR"))
+client = MongoClient(os.environ["MONGO_CONNECTION_STR"],tlsCAFile=certifi.where())
 db = client["sample"]
 
 one_way_hash = lambda x: hashlib.md5(x.encode("utf-8")).hexdigest()
 
+CHAT_VERIFY_COL = "chatverify"
+CHAT_APP_COL = "chatapp"
+
+
+PROMPT = PromptTemplate(template="""
+       Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+       {context}
+       ##Question:{question} \n\
+       ## Chat History: {chat_history}
+       ##AI Assistant Response:\n""",input_variables=["context","chat_history","question"])
 
 def check_doc_in_mdb(md5):
-    if len(list(db["chatverify"].find({"md5": md5}))) > 0:
+    if len(list(db[CHAT_VERIFY_COL].find({"md5": md5}))) > 0:
         return True
     else:
-        db["chatverify"].insert_one({"md5": md5})
         return False
+    
+def insert_doc_verify_mdb(md5):
+    db[CHAT_VERIFY_COL].insert_one({"md5": md5})
 
-
-def get_pdf_data(pdf_docs):
-    for pdf in pdf_docs:
-        text = ""
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-        if check_doc_in_mdb(one_way_hash(text)):
-            return None
-        else:
-            return text
+def get_pdf_data(pdf):
+    text = ""
+    pdf_reader = PdfReader(pdf)
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    md5 = one_way_hash(text)
+    if check_doc_in_mdb(md5):
+        return None, None
+    else:
+        return text,md5
 
 
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=1000,
-        chunk_overlap=200,
+        chunk_overlap=100,
         length_function=len
     )
     chunks = text_splitter.split_text(text)
     return chunks
 
-
-def get_emb_trnsformers():
-    emb_model = "sentence-transformers/all-MiniLM-L6-v2"
-    embeddings_w = HuggingFaceEmbeddings(
-        model_name=emb_model,
-        cache_folder=os.getenv('SENTENCE_TRANSFORMERS_HOME')
-    )
-    return embeddings_w
-
+# def get_embeddings_transformer():
+#     embeddings = VertexAIEmbeddings()
+#     return embeddings
 
 def get_embeddings_transformer():
-    return HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base", model_kwargs={"device": "cpu"})
+    return HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base")
 
-
+@lru_cache(maxsize=1)
 def get_vector_store():
-    col = db["chatapp"]
+    col = db[CHAT_APP_COL]
     vs = MongoDBAtlasVectorSearch(collection=col, embedding=get_embeddings_transformer(), index_name="default",
                                   embedding_key="vec", text_key="line")
     return vs
 
-
-def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
-    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+@lru_cache(maxsize=1)
+def get_conversation_chain():
+    # llm = ChatVertexAI()
+    llm = ChatOpenAI(model="gpt-3.5-turbo")
+    retriever = get_vector_store().as_retriever(search_type="mmr", search_kwargs={'k': 10, 'lambda_mult': 0.25})
+    memory = ConversationBufferWindowMemory(memory_key='chat_history', k=3, return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
+        retriever=retriever,
+        memory=memory,
+        combine_docs_chain_kwargs={"prompt": PROMPT}
     )
+    # conversation_chain = ConversationalRetrievalChain.from_llm(
+    #     llm=llm,
+    #     retriever=retriever,
+    #     memory=memory
+    # )
     return conversation_chain
 
-
 def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chat_history = response['chat_history']
+    conv = st.session_state.conv
+    response = conv({'question': user_question, "chat_history": st.session_state.chat_history})
+    chat_history = [{"user": response["question"], "assistant": response["answer"]}]
+    st.session_state.chat_history += chat_history
     for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            with st.chat_message("user"):
-                st.markdown(message.content)
-        else:
-            with st.chat_message("assistant"):
-                st.markdown(message.content)
+        with st.chat_message("user"):
+            st.markdown(message["user"])
+        with st.chat_message("assistant"):
+            st.markdown(message["assistant"])
+        # if i % 2 == 0:
+        #     with st.chat_message("user"):
+        #         st.markdown(message)
+        # else:
+        #     with st.chat_message("assistant"):
+        #         st.markdown(message.content)
 
 
-def get_llm_chain():
-    # # Locally Hosted LLM
-    # callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-    # llm_llama = LlamaCpp(
-    #     model_path="models/ggml-model-q4_0.bin", callback_manager=callback_manager, verbose=True,
-    #     n_ctx=1024
-    # )
-    llm = OpenAI(model_name="gpt-3.5-turbo-16k", temperature=0.2)
-    chain = RetrievalQA.from_chain_type(llm=llm, retriever=get_vector_store().as_retriever())
-    return chain
 
-
-def main():
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon=":books:")
-
+st.set_page_config(page_title="Chat with multiple PDFs",
+                    page_icon=":books:")
+st.session_state.vectorstore = get_vector_store()
+st.session_state.conv = get_conversation_chain()
+tab1, tab2 = st.tabs(["Q & A", "ADD document"])
+with tab1:        
     st.markdown(
-        """<img src="https://webimages.mongodb.com/_com_assets/cms/kuyjf3vea2hg34taa-horizontal_default_slate_blue.svg?auto=format%252Ccompress" class=" css-1lo3ubz" alt="MongoDB logo" style="height:300px;width:500px;align:center"> """,
+        """<img src="https://lh3.googleusercontent.com/I2_PSO0vMM8kLJxJ-OUIqtSBo3krzhmctqIkFv8Exgchm5X04h_MysTSB-8mELD6J_OIA1N2ExP_=e14-rj-sc0xffffff-h338-w600" class=" css-1lo3ubz" alt="MongoDB logo" style="height:200px;width:340px;align:center"> """,
         unsafe_allow_html=True)
-    st.title("""Your Q&A Assistant for any source powered by Atlas Vector Search """)
+    # st.title("""Assistant for any source powered by Atlas Vector Search and VertexAI""")
+
+    chat_history_clear = st.button("Clear Chat History")
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
+    if ("chat_history" not in st.session_state) or chat_history_clear:
+        st.session_state.chat_history = []
 
-    st.header("Chat with multiple PDFs :books:")
+    st.header("Assistant for any source powered by MongoDB Atlas Vector Search and VertexAI")
     user_question = st.text_input("Ask a question about your documents:")
     if user_question:
+        print(">>>>>>>>>>>>>>>")
         handle_userinput(user_question)
 
+
+with tab2:
+    st.subheader("Your documents")
+    pdf = st.file_uploader(
+        "Upload your PDFs here and click on 'Process'", accept_multiple_files=False)
+    b = st.button("Process")
+    if b:
+        vs = st.session_state.vectorstore
+        with st.spinner("Processing"):
+            # get pdf text
+            raw_text, md5 = get_pdf_data(pdf)
+            if raw_text:
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+                if len(text_chunks)>500:
+                    split = 100
+                else:
+                    split = 10
+                for i in range(0, len(text_chunks), split):
+                    batch_chunks = text_chunks[i:(i + split-1)]
+                    vs.add_texts(batch_chunks)
+                # insert to md5 once indexed
+                insert_doc_verify_mdb(md5)
+                st.write('Document added successfully')
+
     with st.sidebar:
-        st.title("🤗💬 Talk to your Data Powered by LLM's and Vector Search")
+        add_vertical_space(3)
+        st.title("Process your PDFs and perform vector search")
         st.markdown('''
         ## About
         This app is an LLM-powered chatbot built using:
         - [Streamlit](https://streamlit.io/)
         - [LangChain](https://python.langchain.com/)
         - [MongoDB Vector Search](https://www.mongodb.com/products/platform/atlas-vector-search)
-
         ''')
-        add_vertical_space(3)
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_data(pdf_docs)
-
-                if raw_text:
-                    # get the text chunks
-                    text_chunks = get_text_chunks(raw_text)
-
-                    for i in tqdm(range(len(text_chunks), 100)):
-                        batch_chunks = text_chunks[i:i + 99]
-                        # create vector store
-                        get_vector_store().add_texts(text_chunks)
-
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(get_vector_store())
         add_vertical_space(5)
-        st.write('Made with ❤️ by [Ashwin Gangadhar](linkedin.com/in/ashwin-gangadhar-00b17046)')
-
-
-if __name__ == "__main__":
-    main()
+        st.write('Made with ❤️ by [Ashwin Gangadhar](linkedin.com/in/ashwin-gangadhar-00b17046) and [Venkatesh Shanbhag](https://www.linkedin.com/in/venkatesh-shanbhag/)')
